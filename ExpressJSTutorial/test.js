6c2efc8412570996fd4624e5bc8b5e2601f6c097
Abdul Muthalif <XBBLZF0@pac.bnymellon.net>
2017-04-06 21:09:18 +0530

    first commit

---
+++ ExpressJSTutorial/test.js
@@ -0,0 +1,12 @@
+
+var http = require('http');
+
+http.createServer(engine).listen(1337);
+
+function engine(request, response) {
+
+	response.writeHead(200, {'Content-Type':'text/plain'});
+	
+	response.end('hi');
+
+}
